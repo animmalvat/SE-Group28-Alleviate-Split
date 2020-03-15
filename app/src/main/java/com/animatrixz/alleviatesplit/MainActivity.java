90870ee9dfdcfcabb3f5355bdcd4edc237486d55
Anim Yusufbhai Malvat <malvat@uwindsor.ca>
2020-03-15 12:48:29 -0400

    initializing project

---
+++ app/src/main/java/com/animatrixz/alleviatesplit/MainActivity.java
@@ -0,0 +1,81 @@
+package com.animatrixz.alleviatesplit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TESS_DATA = "/tessdata";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/Tess";
+    private TextView textView;
+    private TessBaseAPI tessBaseAPI;
+    private Uri outputFileDir;
+    private String mCurrentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        doOCR();
+    }
+
+
+    private final TessBaseAPI mTess = new TessBaseAPI();
+
+    private void prepareTessData(){
+        try{
+            File dir = getExternalFilesDir(TESS_DATA);
+            if(!dir.exists()){
+                if (!dir.mkdir()) {
+                    Toast.makeText(getApplicationContext(), "The folder " + dir.getPath() + "was not created", Toast.LENGTH_SHORT).show();
+                }
+            }
+            String fileList[] = getAssets().list("");
+            for(String fileName : fileList){
+                String pathToDataFile = dir + "/" + fileName;
+                if(!(new File(pathToDataFile)).exists()){
+                    InputStream in = getAssets().open(fileName);
+                    OutputStream out = new FileOutputStream(pathToDataFile);
+                    byte [] buff = new byte[1024];
+                    int len ;
+                    while(( len = in.read(buff)) > 0){
+                        out.write(buff,0,len);
+                    }
+                    in.close();
+                    out.close();
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    public void doOCR() {
+        prepareTessData();
+        String path = getExternalFilesDir("/").getPath() + "/";
+        mTess.init(path, "eng");
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.image);
+
+        mTess.setImage(bm);
+        String ans = mTess.getUTF8Text();
+        //TextView tv = findViewById(R.id.result);
+        //tv.setText(ans);
+        Log.d("Results", "doOCR: " + ans);
+    }
+}
