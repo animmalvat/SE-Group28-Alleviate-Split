2b9dcf2387a0c5dcc6d0fc44cdb88cb0be4f4c5d
Anim Yusufbhai Malvat <malvat@uwindsor.ca>
2020-04-11 21:02:09 -0400

    add few more images and implemented google android vision api

--- app/src/main/java/com/animatrixz/alleviatesplit/VisionOCR.java
+++ app/src/main/java/com/animatrixz/alleviatesplit/VisionOCR.java
@@ -44,4 +44,24 @@ public class VisionOCR extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vision_o_c_r);
     }
+
+    public void getTextFromImage(View v){
+        Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.raw.image4);
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if(!textRecognizer.isOperational()){
+            Toast.makeText(getApplicationContext(), "Could not get the text", Toast.LENGTH_SHORT).show();
+        } else {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> items = textRecognizer.detect(frame);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < items.size(); i++) {
+                TextBlock myItem = items.valueAt(i);
+                sb.append(myItem.getValue());
+                sb.append("\n");
+            }
+            TextView tv = findViewById(R.id.text);
+            Log.d("image text", "getTextFromImage: "+sb.toString());
+            tv.setText(sb.toString());
+        }
+    }
 }
